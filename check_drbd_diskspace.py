766cd056575f7ac572d45f176b443199620e8712
Izak Burger <isburger@gmail.com>
2013-02-25 10:58:13 +0200

    Check free space, not used space. Fix comments.

--- check_drbd_diskspace.py
+++ check_drbd_diskspace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #
-# This needs nagios-plugins to be installed. It also needs python-argparse, if
-# used with python2.6.
+# This needs python-argparse, if used with python2.6.
 #
 # This is a simple disk space plugin, except that file systems that are not
 # mounted are deemed to be OK - They are mounted somewhere else.
@@ -13,9 +12,9 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--warning", type=int,
-        help="Warning disk space percentage", default=90)
+        help="Warning disk space percentage", default=10)
     parser.add_argument("-c", "--critical", type=int,
-        help="Critical disk space percentage", default=95)
+        help="Critical disk space percentage", default=5)
     parser.add_argument("filesystem", help="The filesystem to check")
     args = parser.parse_args()
 
@@ -24,11 +23,11 @@ def main():
         assert stats.f_blocks > 0, "A file system with zero blocks? Surely you jest?"
         percentage = (stats.f_bfree * 100) / stats.f_blocks
 
-        if percentage > args.critical:
+        if percentage < args.critical:
             print "DISK CRITICAL - free space: %s %d%%" % (args.filesystem,
                 percentage)
             sys.exit(2)
-        elif percentage > args.warning:
+        elif percentage < args.warning:
             print "DISK WARNING - free space: %s %d%%" % (args.filesystem,
                 percentage)
             sys.exit(1)
